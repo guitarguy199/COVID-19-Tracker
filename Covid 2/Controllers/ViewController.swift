ea12fd68ed49ebe6c9d914102a6cb9341d3aa04c
Austin O <guitarguy199@gmail.com>
2021-06-18 12:03:01 -0600

    UI Improvements

--- Covid 2/Controllers/ViewController.swift
+++ Covid 2/Controllers/ViewController.swift
@@ -28,8 +28,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
              xButton.isHidden = true
              xButton.layer.cornerRadius = 0.5 * xButton.bounds.size.width
              xButton.clipsToBounds = true
-     
-             print(pickerData)
     }
     
     
@@ -39,13 +37,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         searchOne.layer.cornerRadius = searchOne.frame.height / 2
         searchButton.layer.cornerRadius = searchButton.frame.height / 2
         allCountriesOutlet.layer.cornerRadius = allCountriesOutlet.frame.height / 2
-        
-        
-        
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -64,18 +55,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
             destinationVC.placeholder = selectedCountry ?? "World"
         }
-         else if segue.identifier == "goToAllCountriesVC" {
-            
-        }
+         else if segue.identifier == "goToAllCountriesVC" {}
     }
     
     @IBOutlet weak var searchButton: UIButton!
     
     
 
-    @IBAction func searchButtonPressed(_ sender: Any) {
-        print("pressed")
-    }
+    @IBAction func searchButtonPressed(_ sender: Any) {}
     
     @IBOutlet weak var searchOne: UIButton!
     @IBOutlet weak var orLabel: UILabel!
@@ -86,14 +73,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
             searchOne.isHidden = true
             searchButton.isHidden = false
             orLabel.isHidden = true
-        xButton.isHidden = false
+            xButton.isHidden = false
             allCountriesOutlet.isHidden = true
-        print("one pressed")
     }
 
 
     
-    //the "x"
+
     @IBAction func xButtonPressed(_ sender: UIButton) {
                 countrySelect.isHidden = true
                 searchOne.isHidden = false
@@ -103,36 +89,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 xButton.isHidden = true
     }
 
-    @IBAction func allCountriesPressed(_ sender: UIButton) {
-    }
+    @IBAction func allCountriesPressed(_ sender: UIButton) {}
     
     @IBOutlet weak var allCountriesOutlet: UIButton!
-    
-//
-//    func createSpinnerView() {
-//        let alert = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
-//
-//        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
-//        loadingIndicator.hidesWhenStopped = false
-//        loadingIndicator.style = UIActivityIndicatorView.Style.medium
-//        loadingIndicator.startAnimating()
-//
-//        alert.view.addSubview(loadingIndicator)
-//        present(alert, animated: true, completion: nil)
-//
-//        let child = SpinnerViewController()
-//        addChild(child)
-//        child.view.frame = view.frame
-//        view.addSubview(child.view)
-//        child.didMove(toParent: self)
-//
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-//            alert.view.removeFromSuperview()
-//            self.performSegue(withIdentifier: "goToAllCountriesVC", sender: self)
-//        }
-//    }
-    
-    
 }
 
 extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
